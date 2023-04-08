c4d2b5d8f4d1571680f4b45ea80f230da762556e
G-RIMA <gikandimaria178@gmail.com>
2023-04-08 09:18:59 +0000

    Task 4 complete: Uploaded image to docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=gamal254/devops
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag devops $dockerpath:latest
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
