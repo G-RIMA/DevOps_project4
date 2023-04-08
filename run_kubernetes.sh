443f3fbf576fc48ba792c10a352994500f867cf6
G-RIMA <gikandimaria178@gmail.com>
2023-04-08 09:53:03 +0000

    deploy with kuberenetes ans save output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="gamal254/devops"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run devops\
+	--image=$dockerpath\
+	--port=80 --labels app=devops
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
+# Step 4:h 
 # Forward the container port to a host
-
+kubectl port-forward devops 8000:80
